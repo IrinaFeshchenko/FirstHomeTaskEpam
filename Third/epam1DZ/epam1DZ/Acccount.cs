08eda8cb82beeb166005c08db9b56c8f6419a9b4
IrinaFeshchenko <58722023+IrinaFeshchenko@users.noreply.github.com>
2019-12-20 06:19:19 +0200

    Add files via upload

---
+++ Third/epam1DZ/epam1DZ/Acccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam1DZ
+{
+    class Acccount
+    {
+        public delegate void AccountHandler(string message);
+        private event AccountHandler _notify;
+        public event AccountHandler Notify
+        {
+            add
+            {
+                _notify += value;
+                Console.WriteLine($"{value.Method.Name} добавлен");
+            }
+            remove
+            {
+                _notify -= value;
+                Console.WriteLine($"{value.Method.Name} удален");
+            }
+        }
+        public Acccount(int sum)
+        {
+            Sum = sum;
+        }
+        public int Sum { get; private set; }
+        public void Put(int sum)
+        {
+            Sum += sum;
+            _notify?.Invoke($"На счет поступило: {sum}");
+        }
+
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                _notify?.Invoke($"Со счета снято: {sum} Текущий баланс: {Sum}");
+
+            }
+            else
+            {
+                _notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {Sum}"); ;
+                Console.Clear();
+            }
+        }
+    }
+}
