c3f5766bc5aa486e1752af0ad300a6faf38e9596
IrinaFeshchenko <58722023+IrinaFeshchenko@users.noreply.github.com>
2019-12-23 01:20:44 +0200

    Add files via upload

---
+++ FourTask/epam1DZ/epam1DZ/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam1DZ
+{
+    public class List
+    {
+        public static void SWAP<T> (ref T x,ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
